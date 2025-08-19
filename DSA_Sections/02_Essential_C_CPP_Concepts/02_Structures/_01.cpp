61c6dcfe2fe2d04da3960142e2e46f71e61c53ec
Krishnendu Ghosh <kai27deep03@gmail.com>
2025-08-19 19:12:02 +0530

    complete structure

--- DSA_Sections/02_Essential_C_CPP_Concepts/02_Structures/_01.cpp
+++ DSA_Sections/02_Essential_C_CPP_Concepts/02_Structures/_01.cpp
@@ -1,1 +1,23 @@
-// Placeholder file for folder structure
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+ struct Rectangle {
+    int l;
+    int w;
+};
+// function to calculate area
+int area(struct Rectangle r) {
+    int c = r.l * r.w;
+    return c;
+}
+int main()
+{
+    struct Rectangle r1={20,7};
+    printf("area previous %d\n", r1.l * r1.w);
+    r1.l = 60;
+    r1.w = 2;
+   // printf("Area current= %d\n", area(r1));
+   cout << "Area current " << area(r1) << endl;
+    printf("Hello world!");
+    return 0;
+}
