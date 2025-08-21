b768fb9bce443215ef0d934eaec903f554591952
Krishnendu Ghosh <kai27deep03@gmail.com>
2025-08-22 00:32:46 +0530

    complete ✅

--- DSA_Sections/02_Essential_C_CPP_Concepts/05_Pointer_to_Structure/_01.cpp
+++ DSA_Sections/02_Essential_C_CPP_Concepts/05_Pointer_to_Structure/_01.cpp
@@ -1,1 +1,24 @@
-// Placeholder file for folder structure
+#include<iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+ struct R {
+  int l;
+  int w;
+ };
+int main()
+{
+   // normal static way
+    struct R r1;
+    r1.l = 20;
+    r1.w = 10;
+    printf("%d\n", r1.l * r1.w);
+   // Dynamic way 
+   struct R *r2;
+  r2 = (struct R*) malloc (sizeof(struct R));
+  r2 -> l = 8;
+  r2 -> w = 10;
+  printf("%d\n", r2 -> l * r2 -> w);
+    cout << "Hello World!" << endl;
+    return 0;
+}
