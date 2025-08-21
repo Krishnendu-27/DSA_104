139415de7959b0faef0abce4d733477cf1bf340a
Krishnendu Ghosh <kai27deep03@gmail.com>
2025-08-21 23:50:48 +0530

    complete ✅

--- DSA_Sections/02_Essential_C_CPP_Concepts/04_Reference_in_CPP/_01.cpp
+++ DSA_Sections/02_Essential_C_CPP_Concepts/04_Reference_in_CPP/_01.cpp
@@ -1,1 +1,18 @@
-// Placeholder file for folder structure
+#include<iostream>
+using namespace std;
+
+int main()
+{ 
+    int b = 15;
+    int &c = b;
+    int *badress;
+    int *cadress;
+    badress = &b;
+    cadress = &c;
+    cout << "the b is " << b << endl;
+    cout << "the adreess b is " << badress << endl;
+    cout << "the adreess c is " << cadress << endl;
+     cout << "the c is " << c << endl;
+    cout << "Hello World!" << endl;
+    return 0;
+}
