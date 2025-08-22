3944acf823b710967408700d7b3508a592b98524
Krishnendu Ghosh <kai27deep03@gmail.com>
2025-08-22 11:17:06 +0530

    complete ✅

--- DSA_Sections/02_Essential_C_CPP_Concepts/06_Functions/_01.cpp
+++ DSA_Sections/02_Essential_C_CPP_Concepts/06_Functions/_01.cpp
@@ -1,1 +1,13 @@
-// Placeholder file for folder structure
+#include<iostream>
+using namespace std;
+int add(int a , int b){
+    return a+b;
+    }
+int main()
+{
+    int x = 10, y = 20;
+    int z = add(x,y);
+    cout << "the answer of z is "<< z << endl;
+    cout << "Hello World!" << endl;
+    return 0;
+}
