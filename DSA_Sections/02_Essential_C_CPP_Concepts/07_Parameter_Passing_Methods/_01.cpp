b1fb81aa87e1f5a39ecd7f899fcbd3bdeba790cf
Krishnendu Ghosh <kai27deep03@gmail.com>
2025-08-23 12:55:55 +0530

    complete ✅

--- DSA_Sections/02_Essential_C_CPP_Concepts/07_Parameter_Passing_Methods/_01.cpp
+++ DSA_Sections/02_Essential_C_CPP_Concepts/07_Parameter_Passing_Methods/_01.cpp
@@ -1,1 +1,41 @@
-// Placeholder file for folder structure
+#include<iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+ void swap_by_refernce(int &x, int &y){
+ int temp;
+ temp = x;
+ x = y;
+ y = temp;
+ }
+ void swap_by_value(int x, int y){
+ int temp;
+ temp = x;
+ x = y;
+ y = temp;
+ }
+ void swap_by_adress(int *x, int* y){
+ int temp;
+ temp = *x;
+ *x = *y;
+ *y = temp;
+ }
+int main()
+{   
+    int x = 1,y=2;
+    int a = 1,b=2;
+    int c = 1,d = 2;
+    cout << "before x is "<< x << " y is " << y << endl;
+    swap_by_value(x,y);
+    cout << "the swap_by_value" << " x is " <<x  << " y is " << y << endl;
+    
+    cout << "before a is "<< a << " b is " << b << endl;
+    swap_by_refernce(a,b);
+    cout << "the swap_by_refernce" << " a is " <<a  << " b is " << b << endl;
+    
+    cout << "before c is "<< c << " d is " << d << endl;
+   swap_by_adress(&c,&d);
+    cout << "the swap_by_adress" << " c is " <<c  << " d is " << d << endl;
+    cout << "Hello World!" << endl;
+    return 0;
+}
