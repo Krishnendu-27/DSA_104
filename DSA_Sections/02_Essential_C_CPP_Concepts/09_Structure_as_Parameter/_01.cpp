d59273694217b7abc2260bf407c8b40cb718fbf0
Krishnendu Ghosh <kai27deep03@gmail.com>
2025-08-26 13:11:52 +0530

    complete ✅

--- DSA_Sections/02_Essential_C_CPP_Concepts/09_Structure_as_Parameter/_01.cpp
+++ DSA_Sections/02_Essential_C_CPP_Concepts/09_Structure_as_Parameter/_01.cpp
@@ -1,1 +1,29 @@
-// Placeholder file for folder structure
+#include<iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+ struct Square{
+     int r;
+ };
+ // call by adress
+ void square3(struct Square *r1){
+     r1->r = r1->r*r1->r*r1->r*r1->r;
+ }
+ // call by value
+ void square1(struct Square r1){
+    r1.r = r1.r*r1.r*r1.r*r1.r;
+ }
+ int square2(struct Square r1){
+    return r1.r = r1.r*r1.r*r1.r*r1.r;
+ }
+int main(){
+    struct Square r={5};
+    printf("in the beganing square of Square is %d\n", r);
+    square1(r);
+    printf("total square1 function of Square is %d\n", r);
+    printf("total square2 function of Square is %d\n", square2(r));
+    square3(&r);
+    printf("total square3 of call by adress function of Square is %d\n", r);
+    cout << "Hello World!" << endl;
+    return 0;
+}
