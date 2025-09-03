6553f93cacdb1c9b3c7ce883bc43392c9919c255
Krishnendu-27 <kai27deep03@gmail.com>
2025-09-03 17:27:59 +0530

    complete recursion basic

--- DSA_Sections/04_Recursion/05_Lets_Code_Recursion/_01.cpp
+++ DSA_Sections/04_Recursion/05_Lets_Code_Recursion/_01.cpp
@@ -1,1 +1,24 @@
 // Placeholder file for folder structure
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+void fun(int n){
+   if(n>0){
+     printf("%d " , n);
+     fun(n-1);
+   }
+}
+void fun2(int n){
+   if(n>0){
+     fun2(n-1);
+     printf("%d " , n);
+   }
+}
+int main() {
+     int x = 5;
+     fun(x);
+     cout << endl;
+     fun2(x);
+    return 0;
+}
--- DSA_Sections/04_Recursion/06_Static_and_Global_Variables_in_Recursion/_01.cpp
+++ DSA_Sections/04_Recursion/06_Static_and_Global_Variables_in_Recursion/_01.cpp
@@ -1,1 +1,26 @@
 // Placeholder file for folder structure
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+int a = 0;
+int fun1(int n){
+    if(n>0){
+        return fun1(n-1)+n;
+    }
+    return 0;
+}
+int fun(int n){
+    static int x = 0;
+    if(n>0){
+        x++;
+        return fun(n-1)+x;
+    }
+    return 0;
+}
+int main() {
+    int x = 5;
+    printf("%d ", fun(x));
+    printf("%d ", fun1(x));
+    return 0;
+}
