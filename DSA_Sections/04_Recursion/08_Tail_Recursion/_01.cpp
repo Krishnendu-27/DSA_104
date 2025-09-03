eeafb270eae217001bf5175c55b347504bcea662
Krishnendu-27 <kai27deep03@gmail.com>
2025-09-03 17:30:18 +0530

    complete ✅

--- DSA_Sections/04_Recursion/08_Tail_Recursion/_01.cpp
+++ DSA_Sections/04_Recursion/08_Tail_Recursion/_01.cpp
@@ -1,1 +1,28 @@
 // Placeholder file for folder structure
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+// using tail recurion
+void fun(int n){
+    if(n>0) {
+        printf("%d ", n);
+        fun(n-1);
+    }
+
+}
+// using loop 
+void fun1(int n){
+   while(n>0){
+    printf("%d ",n);
+    n--;
+    }
+}
+
+int main() {
+    int x = 5;
+    fun(x);
+    cout <<endl;
+    fun1(x);
+    return 0;
+}
