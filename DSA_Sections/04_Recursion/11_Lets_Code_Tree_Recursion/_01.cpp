a906c2c4465b64ea5246eebfe8dfc496dea6d53d
Krishnendu-27 <kai27deep03@gmail.com>
2025-09-04 08:33:49 +0530

    complete Tree recusion

--- DSA_Sections/04_Recursion/11_Lets_Code_Tree_Recursion/_01.cpp
+++ DSA_Sections/04_Recursion/11_Lets_Code_Tree_Recursion/_01.cpp
@@ -1,1 +1,18 @@
 // Placeholder file for folder structure
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+void func(int n){
+if (n>0){
+    printf("%d ", n);
+    func(n-1);
+    func(n-1);
+}
+
+}
+int main() {
+     int x = 4;
+     func(x);
+    return 0;
+}
