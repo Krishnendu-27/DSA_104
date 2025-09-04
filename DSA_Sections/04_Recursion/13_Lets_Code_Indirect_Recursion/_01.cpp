ca7d133f63ef647939e1b0bb62e8310506403025
Krishnendu-27 <kai27deep03@gmail.com>
2025-09-04 09:02:58 +0530

    done indirect recursion

--- DSA_Sections/04_Recursion/13_Lets_Code_Indirect_Recursion/_01.cpp
+++ DSA_Sections/04_Recursion/13_Lets_Code_Indirect_Recursion/_01.cpp
@@ -1,1 +1,26 @@
 // Placeholder file for folder structure
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+// declaration
+void funA(int n);
+void funB(int n);
+//code
+void funA(int n){
+if(n>0){
+    printf("%d ", n);
+    funB(n-1);
+}
+}
+void funB(int n){
+if(n>0){
+    printf("%d ", n);
+    funA(n/2);
+}
+}
+int main() {
+     int x = 20;
+     funA(x);
+    return 0;
+}
