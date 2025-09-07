f90cbffd222b6b9b9addbc35dc948402755c5859
Krishnendu-27 <kai27deep03@gmail.com>
2025-09-07 09:10:32 +0530

    code Taylor_Series_using_Horners_Rule

--- DSA_Sections/04_Recursion/24_Taylor_Series_using_Horners_Rule/_01.cpp
+++ DSA_Sections/04_Recursion/24_Taylor_Series_using_Horners_Rule/_01.cpp
@@ -1,1 +1,20 @@
 // Placeholder file for folder structure
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+double fun(int x, int n){
+    static double S = 1;
+    if(n == 0){
+        return S;
+    }
+    
+        S = 1 + ((x * S) / n);
+    
+    return fun(x,n-1);
+
+}
+int main() {
+     printf("%lf ", fun(5,10));
+    return 0;
+}
