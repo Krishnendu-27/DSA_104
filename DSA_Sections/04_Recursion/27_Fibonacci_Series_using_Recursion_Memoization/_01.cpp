cdaf7f9c8408ed608f9ecf6349f30b706a404c1e
Krishnendu-27 <kai27deep03@gmail.com>
2025-09-07 12:38:50 +0530

    Fibonacci_Series_using_Recursion_Memoization

--- DSA_Sections/04_Recursion/27_Fibonacci_Series_using_Recursion_Memoization/_01.cpp
+++ DSA_Sections/04_Recursion/27_Fibonacci_Series_using_Recursion_Memoization/_01.cpp
@@ -1,1 +1,36 @@
 // Placeholder file for folder structure
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+int F[10];
+int fibo(int n){
+    if (n<=1)
+    {
+        F[n] = n;
+        return n;
+    }
+    else
+    {
+        if (F[n-1]== -1)
+        {
+            F[n-1] = fibo(n-1);
+        }
+        if (F[n-2]== -1)
+        {
+            F[n-2] = fibo(n-2);
+        }
+        return F[n-1] + F[n-2];
+    }
+    
+    
+}
+int main() {
+    for (int i = 0; i < 10; i++)
+    {
+        F[i] = -1;
+    }
+    
+     printf("%d " , fibo(6));
+    return 0;
+}
