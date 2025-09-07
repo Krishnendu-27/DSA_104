6f6db13a666a6d577edb43e20caeb9c47d72f481
Krishnendu-27 <kai27deep03@gmail.com>
2025-09-07 08:50:40 +0530

    Code_Taylor_Series_using_Recursion

--- DSA_Sections/04_Recursion/23_Lets_Code_Taylor_Series_using_Recursion/_01.cpp
+++ DSA_Sections/04_Recursion/23_Lets_Code_Taylor_Series_using_Recursion/_01.cpp
@@ -1,1 +1,22 @@
 // Placeholder file for folder structure
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+double tailor(double x , double n){
+    static double p = 1, f = 1;
+    double r;
+    if(n == 0){
+        return 1;
+    }
+    else{
+        r = tailor(x,n-1);
+        p = p*x;
+        f = f*n;
+        return r + p/f;
+    }
+}
+int main() {
+     printf("%lf" , tailor(4,14));
+    return 0;
+}
