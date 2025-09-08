775ceee10e82d8b9308683f4d3660dbe0faa45da
Krishnendu-27 <kai27deep03@gmail.com>
2025-09-08 09:42:13 +0530

    complete how to incress the size of array

--- DSA_Sections/05_Arrays_Representations/04_Static_vs_Dynamic_Arrays/_01.cpp
+++ DSA_Sections/05_Arrays_Representations/04_Static_vs_Dynamic_Arrays/_01.cpp
@@ -1,1 +1,32 @@
 // Placeholder file for folder structure
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int main() {
+    // static
+    int A[5];
+    printf("the static way\n");
+    for (int i = 0; i < 5; i++)
+    {
+        A[i] = i+2;
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", A[i]);
+    }
+    // dynamic
+    printf("\nthe Dynamic way\n");
+    int *p;
+    p = (int *) malloc(5*sizeof(int));
+    for (int i = 0; i < 5; i++)
+    {
+        p[i] = i*2;
+    }    
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", p[i]);
+    }
+    return 0;
+}
--- DSA_Sections/05_Arrays_Representations/06_How_to_Increase_Array_Size/_01.cpp
+++ DSA_Sections/05_Arrays_Representations/06_How_to_Increase_Array_Size/_01.cpp
@@ -1,1 +1,31 @@
 // Placeholder file for folder structure
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int main() {
+    int *p  = ( int*) malloc(5*sizeof(int));
+    for (int i = 0; i < 5; i++)
+    {
+        p[i] = i+2;
+    }
+    int *q =(int *) malloc(10*sizeof(int));
+    for (int i = 0; i < 5; i++)
+    {
+        q[i] = p[i];
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", q[i]);
+    }
+    free(p);
+    p = q;
+    q = NULL;
+    cout <<endl;
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", p[i]);
+    }
+    return 0;
+}
