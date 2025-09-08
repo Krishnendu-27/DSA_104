89d2e9d619fda757d702b7a2747e1964c22a7b86
Krishnendu-27 <kai27deep03@gmail.com>
2025-09-08 08:45:31 +0530

    Declarations_of_Array

--- DSA_Sections/05_Arrays_Representations/02_Declarations_of_Array/_01.cpp
+++ DSA_Sections/05_Arrays_Representations/02_Declarations_of_Array/_01.cpp
@@ -1,1 +1,28 @@
 // Placeholder file for folder structure
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int main() {
+    int A[5] = {1,2,3,4,5};
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", A[i]);
+    }
+    int B[10];
+    for (int i = 0; i < 10; i++)
+    {
+        B[i] = i+1;
+    }
+    cout <<endl;
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", B[i]);
+    }
+    
+    
+    printf("\n%d" , *(A+2)); 
+    return 0;
+}
+
