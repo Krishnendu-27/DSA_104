4d6bb0901a324027e34721d6a088ae72ef565af2
Krishnendu-27 <kai27deep03@gmail.com>
2025-10-25 11:57:33 +0530

    finalize the fodler structure

---
+++ 01_C-Language/10_Structer/_01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  // structure is a user define datatype 
+  // we need to define a data structure that contain diffrent dtatype
+  
+  return 0;
+}
--- Data-Structer-Algorithm-Using-C/README.md
+++
@@ -1,1 +0,0 @@
-this folder contains all exerise of Data Structer Algorithm Using C exerice and notes to strenhging the abdul bari course and implemenaion all concept
