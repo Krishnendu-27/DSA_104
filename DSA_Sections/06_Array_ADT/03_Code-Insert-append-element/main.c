745980957b4bb389d57d79cd7449393dba500c82
Krishnendu-27 <kai27deep03@gmail.com>
2025-10-25 13:00:28 +0530

    complete aend and inserting element

---
+++ DSA_Sections/06_Array_ADT/03_Code-Insert-append-element/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+struct Array {
+    int A[10];
+    int size;
+    int length;
+};
+
+void display(struct Array * arr) {
+    for (int i = 0; i < arr -> length; i++)
+        printf("%d ", arr -> A[i]);
+    printf("\n");
+}
+
+void append(struct Array * arr, int x) {
+    if (arr -> length < arr -> size) {
+        arr -> A[arr -> length++] = x;
+    } else {
+        printf("Array is full\n");
+    }
+}
+
+void insert(struct Array * arr, int index, int value) {
+    if (index < 0 || index > arr -> length) {
+        printf("Invalid index\n");
+        return;
+    }
+    if (arr -> length == arr -> size) {
+        printf("Array is full\n");
+        return;
+    }
+    for (int i = arr -> length; i > index; i--)
+        arr -> A[i] = arr -> A[i - 1];
+    arr -> A[index] = value;
+    arr -> length++;
+}
+
+int main() {
+    struct Array arr1 = {
+        {
+            1,
+            2,
+            5
+        },
+        10,
+        3
+    };
+    append( & arr1, 9);
+    insert( & arr1, 1, 10);
+    display( & arr1);
+    return 0;
+}
